2125109986974f04affac953558250da6926534f
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2017-12-19 14:45:20 +0100

    added a nice comment feature

--- comments.php
+++ comments.php
@@ -2,36 +2,68 @@
 require __DIR__.'/views/header.php';
 $postId = $_GET['id'];
 $statement = $pdo->query("SELECT * FROM POSTS WHERE post_id = '$postId' ");
-$commentTable = $pdo->query("SELECT USERNAME, comment, comment_date FROM USERS INNER JOIN COMMENTS ON USERS.ID = COMMENTS.user_id WHERE post_id = '$postId' ORDER BY comment_id DESC");
+$commentTable = $pdo->query("SELECT USERNAME, IMAGE , comment, comment_date FROM USERS INNER JOIN COMMENTS ON USERS.ID = COMMENTS.user_id WHERE post_id = '$postId' ORDER BY comment_id DESC");
 $comment = $commentTable->fetchAll(PDO::FETCH_ASSOC);
 $post = $statement->fetch(PDO::FETCH_ASSOC);
 ?>
-<div class="col-xs-4">
-  <H2><?php echo $post['title'];?> </H2>
-  <a href=" <?php echo $post['link']; ?> "> <?php echo $post['link']; ?> <a/> <br>
-  <p><?php echo $post['description'] ?></p>
+<div class="mx-auto col-4">
+<div class="card" style="width: 20rem;">
+  <div class="card-body">
+    <h4 class="card-title"><?php echo $post['title'];?></h4>
+    <hr>
+    <p class="card-text"><?php echo $post['description'] ?>.</p>
+    <hr>
+    <a href=" <?php echo $post['link']; ?> "> <?php echo $post['link']; ?> <a/>
+  </div>
+</div>
+<div class="detailBox">
+  <div class="titleBox">
+    <label>Comments</label>
+  </div>
+  <div class="commentBox">
+    <p class="taskDescription">Remember to read our <a href="#">rules</a> before posting and always be civil!</p>
+  </div>
+  <div class="actionBox">
+    <ul class="commentList">
+<?php foreach ($comment as $key => $value) { ?>
+      <li>
+        <div class="">
+          <p> <?php echo $value['USERNAME']; ?></p>
+        </div>
+        <div class="commenterImage">
+          <img src="<?php
+          if ($value['IMAGE']){
+            echo "images/".$value['IMAGE'];
+          } else {
+            echo "https://s3.amazonaws.com/wll-community-production/images/no-avatar.png";
+          } ?>" />
+        </div>
+        <div class="commentText">
+          <p class=""><?php echo $value['comment'] ?></p> <span class="date sub-text"> <?php echo $value['comment_date'] ?></span>
+        </div>
+      </li>
+      <?php
+    }
+    ?>
+  </ul>
+<?php  if (isset($_SESSION['user'])) { ?>
+  <form class="" action="app/auth/comments.php" method="post">
+    <input type="hidden" name="postId" value="<?php echo $post['post_id'] ?>">
+    <textarea  type="text" name="comment" value=""> </textarea>
+    <button class ="btn btn-outline-secondary" type="submit" name="button">Post comment</button>
+  </form>
 <?php
- foreach ($comment as $key => $value) {
-   echo $value['USERNAME'].": ".$value['comment'].$value['comment_date']."<br>";
- }
-?> </div> <?php
- if (isset($_SESSION['user']['ID'])) { ?>
-      <form class="" action="app/auth/comments.php" method="post">
-        <input type="hidden" name="postId" value="<?php echo $post['post_id'] ?>">
-        <textarea  type="text" name="comment" value=""> </textarea>
-        <button class ="btn btn-outline-secondary" type="submit" name="button">Post comment</button>
-      </form>
-<?php
- if ($post['user_id'] === $_SESSION['user']['ID']) {
-   ?>
-   <a href="editPost.php?id= <?php echo $postId ?> "> <button class="btn btn-outline-secondary" type="submit" name="id" value="<?php echo $value['post_id'] ?>" >EDIT</button> </a>
-   <?php
- }
-}
-if (!isset($_SESSION['user'])){
-  ?><h3>Want to comment?</h3> <?php
-  ?><a href="register.php"> <button class="btn btn-outline-secondary" type="button" name="button">Register!</button> </a><?php
+  if ($post['user_id'] === $_SESSION['user']['ID']) { ?>
+    <a href="editPost.php?id= <?php echo $postId ?> "> <button class="btn btn-outline-secondary" type="submit" name="id" value="<?php echo $value['post_id'] ?>" >EDIT</button> </a>
+    <?php
+  }
+} ?>
+  </div>
+</div>
 
-}
-
+<?php if (!isset($_SESSION['user'])){
+?><h3>Want to comment?</h3> <?php
+?><a href="register.php"> <button class="btn btn-outline-secondary" type="button" name="button">Register!</button> </a><?php
+  }
+?></div><?php
 require __DIR__.'/views/footer.php'; ?>
--- memes.php
+++ memes.php
@@ -2,7 +2,7 @@
 require __DIR__.'/views/header.php';
 if (isset($_SESSION['user'])) {
   ?>
-  <div class="card mx-auto mb-5" style="width: 15rem;">
+  <div class="card mx-auto mb-5 text-center" style="width: 15rem;">
   <div class="card-body">
     <h4 class="card-title">Make a new post!</h4>
     <a href="newPost.php"> <button type="submit" class="btn btn-primary">New post</button></a>
@@ -11,7 +11,7 @@ if (isset($_SESSION['user'])) {
 <?php
 }
 if (!isset($_SESSION['user'])) { ?>
-  <div class="card mx-auto mb-5" style="width: 15rem;">
+  <div class="card mx-auto mb-5 text-center" style="width: 15rem;">
   <div class="card-body">
   <h2>Want to contribute?</h2>
   <a href="register.php"> <button class="btn btn-primary" type="button" name="button">Register!</button> </a>
@@ -28,9 +28,9 @@ foreach ($allPosts as $key => $value) { ?>
   </div>
   <div class="card-body">
     <h4 class="card-title"><?php echo $value['title'];?></h4>
-    <p class="card-text">      <a href=" <?php echo $value['link']; ?> "> <?php echo $value['link']; ?> </a></p>
+    <p class="card-text"><a href=" <?php echo $value['link']; ?> "> <?php echo $value['link']; ?> </a></p>
     <form class="" action="comments.php" method="GET">
-      <a href="comments.php"> <button class="btn btn-primary" type="submit" name="id" value="<?php echo $value['post_id'] ?>" >Comment</button> </a>
+      <a href="comments.php"><button class="btn btn-primary" type="submit" name="id" value="<?php echo $value['post_id'] ?>" >Comment</button></a>
     </form>
   </div>
   <div class="card-footer text-muted">
